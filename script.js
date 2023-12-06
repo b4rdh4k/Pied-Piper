9fe6f3e85d357b77e378a0437cb8ede85384be7c
Bardha Kryeziu <bk68313@ubt-uni.net>
2023-12-06 04:00:09 +0100

    media queries fixed! te about

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-const nav = document.querySelector('nav');
-
-menuIcon.addEventListener('click', () => {
-    nav.classList.toggle('responsive');
-});
