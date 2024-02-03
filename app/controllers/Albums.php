ce6753c0078e2f966d1500cece91b6a08698ac38
bujar5 <bd67972@ubt-uni.net>
2024-02-03 19:55:09 +0100

    ndreqa pak albums.php

---
+++ app/controllers/Albums.php
@@ -0,0 +1,91 @@
+<?php 
+
+/**
+ * home class
+ */
+class Albums
+{
+	use Controller;
+
+    public function create()
+    {
+        if(!$_SESSION['name'])
+        {
+            header('location: users/logIn');
+        }
+
+        if(isset($_POST['album_title']))
+        {
+            $data = [
+                'artist' => $_POST['artist'],
+                'album_title' => $_POST['album_title'],
+                'rating' => $_POST['rating'],
+                'brief_review' => $_POST['brief_review'],
+                'detailed_review' => $_POST['detailed_review'],
+                'genre' => $_POST['genre'],
+                'date_reviewed' => $_POST['date_reviewed'],
+                'label' => $_POST['label'],
+                'albumimage' => $_POST['albumimage'],
+            ];
+            $album_model = new Albums_model;
+
+            $album_model->insert($data);
+
+            return header('location: ../articles');
+        }
+
+        $this->view('articles/createAlbum');
+    }
+
+    public function viewAlbum($id)
+    {
+        if(!$_SESSION['name'])
+        {
+            header('location: users/logIn');
+        }
+
+        $album_model = new Albums_model;
+        $data['artist'] = $album_model->first(['id' => $id]);
+        $this->view('articles/albumview', $data);
+    }
+
+    public function delete($id)
+    {
+        if(!$_SESSION['name'])
+        {
+            header('location: users/logIn');
+        }
+
+        $album_model = new Albums_model;
+        $album_model->delete($id);
+        header('location: ../../articles');
+    }
+
+    public function edit($id){
+        if(!$_SESSION['name'])
+        {
+            header('location: users/logIn');
+        }
+
+        $album_model = new Albums_model;
+        $data['album'] = $album_model->first(['id' => $id]);
+        if(isset($_POST['album_title']))
+        {
+            $data = [
+                'artist' => $_POST['artist'],
+                'album_title' => $_POST['album_title'],
+                'rating' => $_POST['rating'],
+                'brief_review' => $_POST['brief_review'],
+                'detailed_review' => $_POST['detailed_review'],
+                'genre' => $_POST['genre'],
+                'date_reviewed' => $_POST['date_reviewed'],
+                'label' => $_POST['label'],
+                'albumimage' => $_POST['albumimage'],
+            ];
+
+            $album_model->update($id, $data);
+            header('location: ../../artists/viewAlbum/'.$id);
+        }
+        $this->view('articles/albumEdit', $data);
+    }
+}
